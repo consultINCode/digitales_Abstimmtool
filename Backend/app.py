674b0c327ee021c4aedd7d3143c347f847c98685
DennisHallerbach <59708246+DennisHallerbach@users.noreply.github.com>
2020-11-14 12:59:46 +0100

    Template (#7)
    * finalized DBModel Task
    
    * removed shit
    
    * order
    
    * template implemented

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, Response,request
+import logging
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+# Init logging
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+# Some example log lines
+logging.info("test")
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+# this function should be defined in your /api/modelname
+def yourfunction(data="test"):
+    print(data)
+
+# Blueprint for Flask Routes
+# copy and adapt :)
+@app.route('/api/modelname/function', methods=['GET', 'POST','DELETE','PUT'])
+def functionname():
+    if request.method == 'POST':
+        data = request.json
+        yourfunction(data)
+        return Response(status=200)
+    elif request.method == 'GET':
+        return yourfunction()
+    elif request.method == 'PUT':
+        data = request.json
+        yourfunction(data)
+        return Response(status=200)
+    elif request.method == 'DELETE':
+        data = request.json
+        yourfunction(data)
+        return Response(status=200)
+        
+
 
