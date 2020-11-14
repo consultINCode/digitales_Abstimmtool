790057391345449e174f65119e4fd2627fae9d63
DennisHallerbach <59708246+DennisHallerbach@users.noreply.github.com>
2020-11-14 15:58:10 +0100

    Personapi v1

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, Response,request
 import logging
+import api.personapi
+from models import Person, ElectionRound, Choice, session
 app = Flask(__name__)
+
 
 # Init logging
 logging.basicConfig(filename='example.log', level=logging.DEBUG)
@@ -34,5 +37,71 @@ def functionname():
         data = request.json
         yourfunction(data)
         return Response(status=200)
-        
 
+@app.route('/api/persons/getAllPersons', methods =['Get'])
+def getallpersons():
+    if request.method == 'GET':
+        return api.personapi.getAllPersons()
+
+@app.route('/api/persons/getAllPersonsCheckedIn', methods =['Get'])
+def getAllPersonsCheckedIn():
+    if request.method == 'GET':
+        return api.personapi.getAllPersonsCheckedIn()
+
+@app.route('/api/persons/getAllPersonsCheckedOut', methods =['Get'])
+def getAllPersonsCheckedOut():
+    if request.method == 'GET':
+        return api.personapi.getAllPersonsCheckedOut()
+     
+@app.route('/api/persons/createPerson', methods =['POST'])
+def createPerson():
+    if request.method == 'POST':
+        data = request.json
+        if api.personapi.createPerson(data):
+            return Response(status=200)
+        return Response(status= 500)
+
+@app.route('/api/persons/deletePerson', methods =['DELETE'])
+def deletePerson():
+    if request.method == 'DELETE':
+        data = request.json
+        if api.personapi.deletePerson(data):
+            return Response(status=200)
+        return Response(status= 500)
+
+
+# TODO(Test)
+@app.route('/api/persons/approveMinimalVoters', methods =['GET', 'POST'])
+def approveMinimalVoters():
+    if request.method == 'GET':
+        if api.personapi.approveMinimalVoters():
+            return True
+        return False
+    else:
+        return Response(status=405)
+
+@app.route('/api/persons/checkInForElectionRound', methods =['POST'])
+def checkInForElectionRound():
+    if request.method == 'POST':
+        data = request.json
+        if api.personapi.checkInForElectionRound(data):
+            return Response(status=200)
+        return Response(status= 500)
+
+@app.route('/api/persons/checkOutFromElectionRound', methods =['POST'])
+def checkOutFromElectionRound():
+    if request.method == 'POST':
+        data = request.json
+        if api.personapi.checkOutFromElectionRound(data):
+            return Response(status=200)
+        return Response(status= 500)
+
+
+@app.route('/')
+def answer():
+    return "HelloWOrd"
+
+if __name__ == "__main__":
+    app.run()
+
+ 
--- Backend/models.py
+++ Backend/models.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, Text, Boolean
 from sqlalchemy import ForeignKey
 from sqlalchemy import Table
 
+
 engine = create_engine('sqlite:///test.db', echo=False)
 Base = declarative_base()
 
@@ -69,28 +70,56 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+       
+## ADD SOME TEST DATA INTO DB
 
 
+## Person
+p1 = Person()
+p1.name = "Anna"
+p1.password = "hunter2"
+p1.is_present = True
+p1.role = 0
 
+p2 = Person()
+p2.name = "Bob"
+p2.password = "lol123"
+p2.is_present = False
+p2.role = 1
 
-# Relationships Basic How To:
+## Elec-Rounds
+elec_round = ElectionRound()
+elec_round.title = "Braucht Ihr Pause?"
+elec_round.running = "running"
+elec_round.max_choices_per_person = 1
 
-# class Customer(Base):
-#    __tablename__ = 'customers'
+## Choices
+ch1 = Choice()
+ch1.picture = "beer.png"
+ch1.description = "Ja!"
+ch1.counter = 4
+#ch2.elec_round = elec_round
 
-#    id = Column(Integer, primary_key = True)
-#    name = Column(String)
-#    address = Column(String)
-#    email = Column(String)
+ch2 = Choice()
+ch2.picture = "working.jpg"
+ch2.description = "Ne, passt..."
+ch2.counter = 2
+ch2.election_round = elec_round
 
-# class Invoice(Base):
-#    __tablename__ = 'invoices'
-   
-#    id = Column(Integer, primary_key = True)
-#    custid = Column(Integer, ForeignKey('customers.id'))
-#    invno = Column(Integer)
-#    amount = Column(Integer)
-#    customer = relationship("Customer", back_populates = "invoices")
+# RELATIONSHIP (pls work)
 
-# c1 = Customer(name = "Gopal Krishna", address = "Bank Street Hydarebad", email = "gk@gmail.com")
-# c1.invoices = [Invoice(invno = 10, amount = 15000), Invoice(invno = 14, amount = 3850)]
+# The example Choice belongs to the first Election Round 
+ch1.election_round = elec_round
+# Anna and Bob both Voted in the first Election Round
+p1.voted_in_election_round.append(elec_round)
+p2.voted_in_election_round.append(elec_round)
+# Anna has Bobs Vote
+p1.received_proxy_vote.append(p2)
+
+session.add(p1)
+session.add(p2)
+session.add(elec_round)
+session.add(ch1)
+session.add(ch2)
+
+session.commit()
