4749672c2f34b9b1946123ee94652340ff22e1a5
Dominik Sigl <dominik.schlecht@hotmail.de>
2020-11-15 11:25:42 +0100

    Sceleton standing

---
+++ Backend/api/voteapi.py
@@ -0,0 +1,19 @@
+from models import session
+import base64
+import json
+
+def setVote(elec_round, person):
+    '''Person hat erfolgreich für diese Wahl abgestimmt'''
+    # TODO(Impl method)
+    pass    
+
+def getAllPersonsWhoVoted(elec_round):
+    '''Gibt alle Personen zurück die in der Wahlrunde schon gewählt haben'''
+    # TODO(Impl method)
+    pass    
+
+def getAllPersonsWhoHaveNotVoted(elec_round):
+    '''Gibt alle Personen zurück die noch Nicht gewählt haben'''
+    # TODO(Impl method)
+    pass
+
