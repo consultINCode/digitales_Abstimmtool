a68ddbcecdd66e0bca90df593b9753bf69bc261a
DennisHallerbach <59708246+DennisHallerbach@users.noreply.github.com>
2020-11-15 22:38:23 +0100

    first swagger implementation

--- Backend/app.py
+++ Backend/app.py
@@ -25,7 +25,7 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+        'app_name': "consultin Abstimmungstool"
     }
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
