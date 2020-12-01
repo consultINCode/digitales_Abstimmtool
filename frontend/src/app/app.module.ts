97d718ff9ac3c240c4c32757423be0b288f1ba76
Pascal Strack <pascal.strack@outlook.de>
2020-12-01 22:57:11 +0100

    adjust header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,26 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {environment} from '../environments/environment';
 // App component
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 // own modules
 import {PersonModule} from './person/person.module';
 import {LandingPageModule} from './landing-page/landing-page.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ElectionModule} from './election/election.module';
+import {TopNavBarComponent} from "./core/components/top-nav-bar/top-nav-bar.component";
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopNavBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     // own Modules
     PersonModule,
     LandingPageModule,
