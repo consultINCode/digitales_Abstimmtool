9f3314a57d0e9a258c35a559730da179d941855a
Dennis Hallerbach <dennis.hallerbach@hs-augsburg.de>
2021-04-04 16:26:02 +0200

    implemented blueprints and checked some implementation

---
+++ Backend/blueprints/voteblueprint.py
@@ -0,0 +1,26 @@
+from flask import Blueprint,request
+from api import voteapi
+
+vote_Blueprint = Blueprint('vote_Blueprint', __name__)
+
+
+#GET: electionroundid=<number>
+#TODO Test Method!
+#RETURNS: { "id":<number>, "name":<string> }
+@vote_Blueprint.route('/api/election/getAllPersonsWhoVoted/<electionroundid>', methods =['GET'])
+def get_all_persons_who_voted(electionroundid):
+    return voteapi.get_all_persons_who_voted(electionroundid)
+
+#GET: electionroundid=<number>
+#TODO Test Method!
+#RETURNS: { "id":<number>, "name":<string> }
+@vote_Blueprint.route('/api/vote/getAllPersonsWhoHaveNotVoted/<electionroundid>', methods =['GET'])
+def get_all_persons_who_have_not_voted(electionroundid):
+    return voteapi.get_all_persons_who_have_not_voted(electionroundid)
+
+#POST: { "elec_round_id":<number>, "person_id":<number> }
+#RETURNS: { "Result?":<string>, "Error?":<string> }
+@vote_Blueprint.route('/api/vote/setVote', methods =['POST'])
+def set_vote():       
+    return voteapi.set_vote(request.json)
+
